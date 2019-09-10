c66ea7c12ce72c7685d5601f576e1a3e865347dd
OsmanIzhar <osmanizhaar@gmail.com>
2019-09-10 21:20:58 +0500

    changes

--- ERP/Accounts/RPT/AC01.cs
+++ ERP/Accounts/RPT/AC01.cs
@@ -70,7 +70,6 @@ namespace ERP.Accounts.RPT
         {
             rptAC01 clsrptAC01 = new rptAC01();
             DataSet ds_1;
-
             ds_1 = clsrptAC01.Fill_Combo(mSys_System.pComp_ID, mSys_System.pUser_ID);
             if (clsrptAC01.sErrorCode.Trim() != "")
             {
